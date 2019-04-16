0b5795b7578031029b363c69296c5699678961df
Marco Lewandowski <lewandowski@swms.de>
2019-04-16 12:55:36 +0200

    Projektdateien hinzufügen.

---
+++ UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b25e9cab-60a0-42f4-9c51-48921bdbc0ec")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string password = "yxcvbnm";
+            string plaintext = "This is my string, not encrypted.";
+
+            Console.WriteLine("Your encrypted string is:");
+            string encryptedstring = StringCipher.StringCipher.Encrypt(plaintext, password);
+            Console.WriteLine(encryptedstring);
+
+            Assert.AreNotEqual(plaintext, encryptedstring);
+
+
+            Console.WriteLine("Your decrypted string is:");
+            string decryptedstring = StringCipher.StringCipher.Decrypt(encryptedstring, password);
+            Console.WriteLine(decryptedstring);
+
+            Assert.AreEqual(plaintext, decryptedstring);
+        }
+    }
+}
